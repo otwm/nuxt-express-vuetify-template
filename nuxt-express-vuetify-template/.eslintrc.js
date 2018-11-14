4de8828d1d3dbd27701a8916636a45ac83188285
dong o kim <otwmoo@gmail.com>
2018-11-14 15:52:17 +0900

    # eslint

--- nuxt-express-vuetify-template/.eslintrc.js
+++ nuxt-express-vuetify-template/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'eslint:recommended',
     'plugin:vue/essential'
   ],
   // required to lint *.vue files
@@ -17,5 +18,16 @@ module.exports = {
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    "code": 120,
+    "comma-dangle": ["error", {
+      "arrays": "always",
+      "objects": "always",
+      "imports": "ignore",
+      "exports": "ignore",
+      "functions": "never"
+    }],
+    "quotes": [2, "single", { "avoidEscape": true }],
+    "no-console": "off"
+  }
 }
--- nuxt-express-vuetify-template/server/index.js
+++ nuxt-express-vuetify-template/server/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import { Nuxt, Builder } from 'nuxt'
+import express from 'express';
+import { Nuxt, Builder } from 'nuxt';
 
-const app = express()
-const host = process.env.HOST || '127.0.0.1'
-const port = process.env.PORT || 3000
+const app = express();
+const host = process.env.HOST || '127.0.0.1';
+const port = process.env.PORT || 3000;
 
-app.set('port', port)
+app.set('port', port);
 
-let config = require('../nuxt.config.js')
-config.dev = !(process.env.NODE_ENV === 'live')
+let config = require('../nuxt.config.js');
+config.dev = !(process.env.NODE_ENV === 'live');
 
-const nuxt = new Nuxt(config)
+const nuxt = new Nuxt(config);
 
 if (config.dev) {
-  const builder = new Builder(nuxt)
-  builder.build()
+  const builder = new Builder(nuxt);
+  builder.build();
 }
 
-app.use(nuxt.render)
-app.listen(port, host)
+app.use(nuxt.render);
+app.listen(port, host);
 
-console.log('Server listening on ' + host + ':' + port)
+console.log('Server listening on ' + host + ':' + port);
