5472a813d8498e6faac912056af2b11ea64e10af
dong o kim <otwmoo@gmail.com>
2018-11-14 14:54:19 +0900

    # express 설치 * backpack 활용

---
+++ nuxt-express-vuetify-template/server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { Nuxt, Builder } from 'nuxt'
+
+const app = express()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+app.set('port', port)
+
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'live')
+
+const nuxt = new Nuxt(config)
+
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
+}
+
+app.use(nuxt.render)
+app.listen(port, host)
+
+console.log('Server listening on ' + host + ':' + port)
